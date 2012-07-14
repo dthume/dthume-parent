e2ff97cd5d788ab049d51554d1be4e004a6b5de4
David Thomas Hume <dth@dthu.me>
2012-07-14 21:31:04 +0100

    Move to jacoco 0.5.8

---
+++ dthume-jar-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+package ${package};
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldTestSomething() throws Exception {
+        fail("todo");
+    }
+}
